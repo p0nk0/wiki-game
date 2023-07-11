7c7df51e45a538310d6872ea6a37f956f7dd382e
Taylor Roberts <roberts.taylor000@gmail.com>
2023-07-11 21:09:10 +0000

    finished exercise 2

--- src/interstate.ml
+++ src/interstate.ml
@@ -1,5 +1,58 @@
 open! Core
 
+(* A LOT OF THIS IS COPIED FROM SOCIAL_NETWORK *)
+
+module City = String
+
+module Highway = struct
+  include String
+
+  let default = "default?"
+end
+
+(* We separate out the [Network] module to represent our social network in
+   OCaml types. *)
+module Network = struct
+  (* We can represent our social network graph as a set of connections, where
+     a connection represents a friendship between two people. *)
+  module Connection = struct
+    module T = struct
+      type t = City.t * Highway.t * City.t [@@deriving compare, sexp]
+    end
+
+    (* This funky syntax is necessary to implement sets of [Connection.t]s.
+       This is needed to defined our [Network.t] type later. Using this
+       [Comparable.Make] functor also gives us immutable maps, which might
+       come in handy later. *)
+    include T
+    include Comparable.Make (T)
+
+    let of_string_helper cities =
+      let hwy = List.hd_exn cities in
+      let cities_trimmed =
+        List.tl_exn cities
+        |> List.map ~f:(Str.global_replace (Str.regexp {|\.|}) "")
+        |> List.map ~f:(Str.global_replace (Str.regexp {| |}) "_")
+      in
+      List.cartesian_product cities_trimmed cities_trimmed
+      |> List.filter_map ~f:(fun (a, b) ->
+           if String.equal a b then None else Some (a, hwy, b))
+    ;;
+
+    let of_string s = String.split s ~on:',' |> of_string_helper
+  end
+
+  type t = Connection.Set.t [@@deriving compare, sexp_of]
+
+  let of_file input_file =
+    let connections =
+      In_channel.read_lines (File_path.to_string input_file)
+      |> List.concat_map ~f:Connection.of_string
+    in
+    Connection.Set.of_list connections
+  ;;
+end
+
 let load_command =
   let open Command.Let_syntax in
   Command.basic
@@ -9,25 +62,61 @@ let load_command =
         flag
           "input"
           (required File_path.arg_type)
-          ~doc:"FILE a file listing interstates and the cities they go through"
+          ~doc:
+            "FILE a file listing interstates and the cities they go through"
       in
       fun () ->
-        ignore (input_file : File_path.t);
-        failwith "TODO"]
+        (* this is copied from social_network file *)
+        let network = Network.of_file input_file in
+        (* This special syntax can be used to easily sexp-serialize values
+           (whose types have [sexp_of_t] implemented). *)
+        printf !"%{sexp: Network.t}\n" network]
 ;;
+
+(* MORE COPIED STUFF *)
+
+(* In order to visualize the social network, we use the ocamlgraph library to
+   create a [Graph] structure whose vertices are of type [Person.t].
+
+   The ocamlgraph library exposes lots of different ways to construct
+   different types of graphs. Take a look at
+   https://github.com/backtracking/ocamlgraph/blob/master/src/imperative.mli
+   for documentation on other types of graphs exposed by this API. *)
+module G = Graph.Imperative.Graph.ConcreteLabeled (City) (Highway)
+
+(* We extend our [Graph] structure with the [Dot] API so that we can easily
+   render constructed graphs. Documentation about this API can be found here:
+   https://github.com/backtracking/ocamlgraph/blob/master/src/dot.mli *)
+module Dot = Graph.Graphviz.Dot (struct
+  include G
+
+  (* These functions can be changed to tweak the appearance of the generated
+     graph. Check out the ocamlgraph graphviz API
+     (https://github.com/backtracking/ocamlgraph/blob/master/src/graphviz.mli)
+     for examples of what values can be set here. *)
+  let edge_attributes _ = [ `Dir `None ]
+  let default_edge_attributes _ = []
+  let get_subgraph _ = None
+  let vertex_attributes v = [ `Shape `Box; `Label v; `Fillcolor 1000 ]
+  let vertex_name v = v
+  let default_vertex_attributes _ = []
+  let graph_attributes _ = []
+end)
 
 let visualize_command =
   let open Command.Let_syntax in
   Command.basic
     ~summary:
-      "parse a file listing interstates and generate a graph visualizing the highway \
-       network"
+      "parse a file listing interstates and generate a graph visualizing \
+       the highway network"
     [%map_open
       let input_file =
         flag
           "input"
           (required File_path.arg_type)
-          ~doc:"FILE a file listing all interstates and the cities they go through"
+          ~doc:
+            "FILE a file listing all interstates and the cities they go \
+             through"
       and output_file =
         flag
           "output"
@@ -35,8 +124,15 @@ let visualize_command =
           ~doc:"FILE where to write generated graph"
       in
       fun () ->
-        ignore (input_file : File_path.t);
-        ignore (output_file : File_path.t);
+        let network = Network.of_file input_file in
+        let graph = G.create () in
+        Set.iter network ~f:(fun (city1, hwy, city2) ->
+          (* [G.add_edge] auomatically adds the endpoints as vertices in the
+             graph if they don't already exist. *)
+          G.add_edge_e graph (city1, hwy, city2));
+        Dot.output_graph
+          (Out_channel.create (File_path.to_string output_file))
+          graph;
         printf !"Done! Wrote dot file to %{File_path}\n%!" output_file]
 ;;
 
--- src/interstate.mli
+++ src/interstate.mli
@@ -3,5 +3,4 @@ open! Core
 (** Commands for interacting with data about interstate highways.
 
     For an example of an input file, see ../resources/interstate.txt *)
-
 val command : Command.t
